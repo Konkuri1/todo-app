3f605021ee52330b9c54e87260b42511885b0691
Konkuri1 <konkurievarest363@gmail.com>
2024-02-21 16:05:11 +0000

    bootstrapProps

--- src/components/add-todo/index.js
+++ src/components/add-todo/index.js
@@ -21,12 +21,15 @@ function AddTodo() {
         // todos.push(todo);
         // Set all todos in local storage
         setTodos([...todos, todo]);
+        // wipe input
+        setTodo("");
     }
 
 
     return (
         <section className={styles.addTodo}>
             <input
+                value={todo}
                 onChange={collectInput}
                 className={styles.addTodoInput}
                 placeholder="Start typing..." />
--- src/components/todo-list/index.js
+++ src/components/todo-list/index.js
@@ -1,15 +1,23 @@
 import { useLocalStorage } from "usehooks-ts";
+import TodoItem from "../todo-item";
 
 function TodoList() {
-    const [todos, setTodos] = useLocalStorage("TODO_KEY", []);
+  const [todos, setTodos] = useLocalStorage("TODO_KEY", []);
 
-    return (
-        <ul>
-            {todos.map(function (todo, index) {
-                return <li key={index}>{todo}</li>
-            })}
-        </ul>
-    );
+  function deleteAll () {
+    setTodos([]);
+  }
+
+  return (
+    <section>
+        <button onClick={deleteAll} className="btn btn-danger"> Delete All</button>
+        <ul className="list-group">
+      {todos.map(function (todo, index) {
+        return <TodoItem todo={todo} index={index} />;
+      })}
+    </ul>
+    </section>
+  );
 }
 
 export default TodoList;
